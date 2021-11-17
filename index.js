63cc6ae51ff05fd97198bde2f66e03000dbbbe63
Nagaswarup96 <nagaswarupdp96@gmail.com>
2021-11-17 11:58:12 +0530

    deleted files

--- index.js
+++
@@ -1,5 +0,0 @@
-const handleClick = () => {
-  const val = document.getElementById("inpVal").value;
-  document.getElementById("total").innerText = val.length;
-  document.getElementById("total").style.display = "block";
-};
