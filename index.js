ea761918321d82517c0727654f0d0733d3030bcd
Nagaswarup96 <nagaswarupdp96@gmail.com>
2021-11-23 11:48:04 +0530

    quote added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector("#btn");
-const h1 = document.getElementById("author");
+const h1 = document.getElementById("quote");
+const h3 = document.getElementById("author");
 const url = "https://breakingbadapi.com/api/quote/random";
 
 const data = async () => {
   try {
     const response = await fetch(url);
     const values = await response.json();
-    h1.innerText = values[0].author;
+    h1.innerText = values[0].quote;
+    h3.innerText = values[0].author;
   } catch (err) {
     console.log(err);
   }
